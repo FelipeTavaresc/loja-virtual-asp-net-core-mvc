ed36c898a6caf3b2378c6721faebacc2f557410b
Felipe Tavares <felipe.tavares@outlook.com>
2020-01-13 21:08:25 -0300

    Cadastro de Cliente -

---
+++ LojaVirtual/Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LojaVirtual.Database;
+using LojaVirtual.Libraries.Email;
+using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Controllers
+{
+    public class HomeController : Controller
+    {
+        private LojaVirtualContext _banco;
+        public HomeController(LojaVirtualContext banco)
+        {
+            _banco = banco;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index([FromForm]NewLetterEmail newLetter)
+        {
+            if (ModelState.IsValid)
+            {
+                _banco.NewLetterEmails.Add(newLetter);
+                _banco.SaveChanges();
+
+                TempData["MSG_S"] = "E-mail cadastrado com sucesso";
+
+                return RedirectToAction(nameof(Index));
+            }else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Contato()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            try
+            {
+                Contato contato = new Contato();
+                contato.Nome = HttpContext.Request.Form["nome"];
+                contato.Email = HttpContext.Request.Form["email"];
+                contato.Texto = HttpContext.Request.Form["texto"];
+
+                var listaMensagens = new List<ValidationResult>();
+                var contexto = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, contexto, listaMensagens, true);
+
+                if (isValid)
+                {
+                    ContatoEmail.EnviarContatoPorEmail(contato);
+
+                    ViewData["MSG_S"] = "Mensagem enviada com sucesso";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach (var texto in listaMensagens)
+                    {
+                        sb.Append(texto.ErrorMessage + "<br />");
+                    }
+
+                    ViewData["MSG_E"] = sb.ToString();
+                    ViewData["CONTATO"] = contato;
+                }
+
+                
+            }
+            catch (Exception e)
+            {
+                ViewData["MSG_E"] = "Ocorreu um erro, tente mais tarde!";
+
+                //TODO - Implementar log
+            }
+
+            return View("Contato");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadastroCliente([FromForm]Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _banco.Add(cliente);
+                _banco.SaveChanges();
+
+                TempData["MSG_S"] = "Cadastro realizado com sucesso";
+
+                return RedirectToAction(nameof(CadastroCliente));
+            }
+
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+    }
+}
---
+++ LojaVirtual/Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,40 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            SmtpClient smtp = new SmtpClient("SMTP.office365.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("felipe.tavares@outlook.com", "");
+            smtp.EnableSsl = true;
+
+            string corpoMsg = string.Format("<h2>Contato - Loja Virtual</h2>" +
+                "<b>Nome: </b> {0} <br />" +
+                "<b>E-mail: </b> {1} <br />" +
+                "<b>Texto: </b> {2} <br />" +
+                "<br /> E-mail enviado automaticamente do site Loja Virtual",
+                contato.Nome,
+                contato.Email,
+                contato.Texto
+           );
+
+            MailMessage mensagem = new MailMessage();
+            mensagem.From = new MailAddress("felipe.tavares@outlook.com");
+            mensagem.To.Add("felipe.tavares@outlook.com");
+            mensagem.Subject = "Contato - Loja Virtual - E-mail:" + contato.Email;
+            mensagem.Body = corpoMsg;
+            mensagem.IsBodyHtml = true;
+
+            smtp.Send(mensagem);
+        }
+    }
+}
