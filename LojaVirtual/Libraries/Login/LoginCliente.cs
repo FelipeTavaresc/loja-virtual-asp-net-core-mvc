fb65d6a23b09f2b36007e26508b70d044c3acb7f
Felipe Tavares <felipe.tavares@outlook.com>
2020-05-28 19:14:58 -0300

    Sessão - Classe LoginCliente

---
+++ LojaVirtual/Libraries/Login/LoginCliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojaVirtual.Libraries.Sessao;
+using LojaVirtual.Models;
+using Newtonsoft.Json;
+
+namespace LojaVirtual.Libraries.Login
+{
+    public class LoginCliente
+    {
+        private string Key = "LoginCliente";
+        private Sessao.Sessao _sessao;
+        public LoginCliente(Sessao.Sessao sessao)
+        {
+            _sessao = sessao;
+        }
+
+        public void Login(Cliente cliente)
+        {
+            string clienteJSONString = JsonConvert.SerializeObject(cliente);
+            _sessao.Cadastrar(Key, clienteJSONString);
+        }
+
+        public Cliente GetCliente()
+        {
+            string clienteJSONString = _sessao.Consultar(Key);
+            return JsonConvert.DeserializeObject<Cliente>(clienteJSONString);
+        }
+
+        public void Logout()
+        {
+            _sessao.RemoverTodos();
+        }
+    }
+}
