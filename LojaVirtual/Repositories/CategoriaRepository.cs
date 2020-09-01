64e251e3e4aa197656302bf4098d0f2b9ae79be3
Felipe Tavares <felipe.tavares@outlook.com>
2020-09-01 20:29:41 -0300

    Area Colaborador - Categoria Pai

--- LojaVirtual/Repositories/CategoriaRepository.cs
+++ LojaVirtual/Repositories/CategoriaRepository.cs
@@ -1,6 +1,7 @@
 ﻿using LojaVirtual.Database;
 using LojaVirtual.Models;
 using LojaVirtual.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -46,7 +47,7 @@ namespace LojaVirtual.Repositories
         public IPagedList<Categoria> ObterTodasCategorias(int? pagina)
         {
             int numeroPagina = pagina ?? 1;
-            return _banco.Categorias.ToPagedList<Categoria>(numeroPagina, _registroPorPagina);
+            return _banco.Categorias.Include(a => a.CategoriaPai).ToPagedList<Categoria>(numeroPagina, _registroPorPagina);
         }
     }
 }
